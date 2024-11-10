9408268f4aa21a809d6e071b03378bc9a4cd31e7
olivermaketso <olivermaketso@gmail.com>
2024-11-10 20:40:29 +0300

    debugging

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+hash_password returns a salted, hashed password, byte in string 
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt with a salt.
+    Args:
+        password (str): The plaintext password to hash.
+    Returns:
+        bytes: The salted and hashed password.
+    """
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password_bytes, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate provided password matched hashed_password"""
+    bytes = password.encode('utf-8')
+    return bcrypt.checkpw(bytes, hashed_password)
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""Module with a function called filter_datum"""
+import logging
+import re
+from typing import List
+import mysql.connector
+import os
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Obfuscates the fields in the log message.
+
+    Args:
+        fields (List[str]): Fields to obfuscate.
+        redaction (str): The string to replace the field values.
+        message (str): The log message.
+        separator (str): The separator used in the log message.
+
+    Returns:
+        str: The obfuscated log message.
+    """
+    for field in fields:
+        message = re.sub(f"{field}=(.*?){separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ 
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records using filter_datum."""
+        log_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            log_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Creates a logger named user_data with specific configurations"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connecting to MySQL using environment variables"""
+    db_connect = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return db_connect
+
+
+def main() -> None:
+    """Obtains a database connection using get_db and retrieve all rows in
+    the users table and display each row under a filtered format."""
+    logger = get_logger()
+    logger.setLevel(logging.INFO)
+
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    rows = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+
+    for row in rows:
+        message = "; ".join([f"{columns[i]}={row[i]}"
+                             for i in range(len(columns))])
+        logger.info(filter_datum(PII_FIELDS, RedactingFormatter.REDACTION,
+                                 message, RedactingFormatter.SEPARATOR))
+
+
+if __name__ == "__main__":
+    main()
